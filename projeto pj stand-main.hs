5c24447f9d6edd2529d88e7216355396e5c541c7
DiogoManuel <42673788+DiogoManuel@users.noreply.github.com>
2019-05-06 00:25:06 +0100

    Add files via upload

---
+++ projeto pj stand-main.hs
@@ -0,0 +1,20 @@
+menu:: IO ()
+
+menu = do 
+           putStrLn a
+                      where a= "\t\t\t\tPJ-STAND"++
+                               "\n"++
+                               "\n"++
+                               "\n"++
+                               "\t\t1- Ver Precario"++
+                               "\n" ++"\t\t2- Listar Produtos"++
+                               "\n"++"\t\t3- Efectuar Compra"++
+                               "\n"++"\t\t4- Consultar Factura"++
+                               "\n"++"\t\t5- Ver Facturas Diarias"++
+                               "\n"++ "\t\t6- Ver Todas As Facturas"++
+                               "\n"++ "\t\t7- Automovel mais vendido"++
+                               "\n"++"\t\t8- Sair"++
+                               "\n"++"\n"
+            --b <- getLine
+                           
+		   

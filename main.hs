e2c08e59507c6c7419be2a4e97c574d9caeea1f3
Celestino <celestinolanga11@gmail.com>
2019-05-05 13:50:28 +0100

    Menu
    a função menu já está feita

---
+++ main.hs
@@ -0,0 +1,13 @@
+module Princioal where
+
+main::IO()
+main = do
+            putStr "-------------- Menu ------------------\n"
+            putStr "1- Ver Preçario\n"
+            putStr "2- Listar Produtos\n"
+            putStr "3- Efectuar Compra\n"
+            putStr "4- Consultar Factura\n"
+            putStr "6- Ver Factura Diária\n"
+            putStr "7- Mostrar as informações do automóvel mais vendido\n"
+            putStr "8- Sair\n *Selecione a Tarefa que deseja realizar: "
+

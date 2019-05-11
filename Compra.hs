61c34f2a75fe3fc97d0b2f820059a50337c4ae71
Ricardo Neves <ricardodcmeloneves@gmail.com>
2019-05-11 15:36:51 +0100

    Ja pode-se adicionar varias linhas de factura

--- Compra.hs
+++ Compra.hs
@@ -10,17 +10,8 @@ compra = do
                 nome <- getLine
                 putStrLn ("Informe o Nº de Telefone do Cliente:")
                 telefone <- readLn :: IO Int
-                putStrLn("Selecine o Carro ou Mota que Deseja Comprar\nCarros:\n")
-                putStrLn(exibeCarros add Carro)
-                putStrLn("\n\nMota:")
-                putStrLn(exibeCarros add Moto)
-                putStr("Digite o codigo do Automovel que deseja: ")
-                codigo<-getLine
-                if((existe add (read codigo))== True) then do
-                    putStr("tem")
-                    else 
-                        putStr ("Não existe")
-                       
+                comprarCarro []
+                putStr("")
                 else
                     putStr ("nao")
             
@@ -37,3 +28,42 @@ exibeCarros ((cod,nome,preco,categoria,marca,tipo):xs) tipoComp= if(tipo == tipo
         else
             exibeCarros xs tipoComp
 
+
+retornaPreco ((c,_,preco,_,_,_):xs) cod | c == cod = preco
+                                        | otherwise = retornaPreco xs cod
+
+addLinhaFactura::Int->Double->Int->Double->(Int,Double,Int,Double)
+addLinhaFactura cod preco qtd subtotal = (cod,preco,qtd,subtotal)
+
+addFactura [] elemento = [elemento]
+addFactura (x:xs) elemento = (x:xs)++[elemento]
+
+
+listaLinhas = []
+
+
+comprarCarro lista = do                 
+                putStrLn("Selecine o Carro ou Mota que Deseja Comprar\nCarros:\n")
+                putStrLn(exibeCarros add Carro)
+                putStrLn("\n\nMota:")
+                putStrLn(exibeCarros add Moto)
+                putStr("Digite o codigo do Automovel que deseja: ")
+                codigo<-getLine
+                if((existe add (read codigo))== True) then do
+                    putStrLn ("Digite a Quantidade que deseja: ")
+                    qtd<-getLine
+                    
+                 
+                    putStrLn(show (lista++[(addLinhaFactura (read codigo) (retornaPreco add (read codigo)) (read qtd) ((read qtd) * (retornaPreco add (read codigo))))]  )) 
+                    
+                    putStrLn("Adicionado ao Carrinho")
+                    putStrLn("1-Concluir Compra\n2-Adicionar as Compra")
+                    valor<-getLine
+                    case (read valor) of
+                        1-> putStr("Conclui")
+                            
+                        _-> comprarCarro (lista++[(addLinhaFactura (read codigo) (retornaPreco add (read codigo)) (read qtd) ((read qtd) * (retornaPreco add (read codigo))))]  ) 
+                    
+                   
+                    else 
+                        putStr ("Não existe")

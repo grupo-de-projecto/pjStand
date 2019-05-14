ff27af2bd055c6a7113e54e5213e3a2a9cba5f95
Celestino <celestinolanga11@gmail.com>
2019-05-14 16:14:18 +0100

    Trabalhar na minha parte

--- Dados.hs
+++ Dados.hs
@@ -2,19 +2,45 @@ module Dados  where
 
 import Estrutura
 import Data.List.Split
+import System.IO
 
 
 dados::Automoveis
 dados = [(1,"Celestino",10,Ligeiro,Toyota,Carro), (1,"Langa",10,Ligeiro,Toyota,Carro)]
 
-{-escrever::IO()
+escrever::IO()
 escrever = do
-            writeFile "info.txt" "Testar"
-            putStrLn "Dados escrito"-}
-            
-ler::IO [String]
-ler = do
-        a <- readFile "dados.txt"
-        return (endBy ";" (show a))
+            appendFile "dados.txt" (show dados)
+            putStrLn "Dados escrito"
 
+
+rtn_pri (a,_,_,_,_,_) = a
+rtn_sgn (_,a,_,_,_,_) = a
+rtn_ter (_,_,a,_,_,_) = a
+rtn_qrt (_,_,_,a,_,_) = a
+rtn_qnt (_,_,_,_,a,_) = a
+rtn_ult (_,_,_,_,_,a) = a
+
+
+
+
+
+ler = do
+        a <- openFile "dados.txt" ReadMode
+        conteudo <- hGetContents a
+        return conteudo
+        hClose a
         
+f :: String -> Int
+f = read
+
+io :: IO String
+io = getLine
+
+main :: IO ()
+main = (f `fmap` io) >>= \x -> print x
+
+
+{-ler1::IO [String]->[String]
+ler1  l= do
+        return [l]-}

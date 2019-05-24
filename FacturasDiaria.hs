3afe1957ffd8a18cb57512b8bd8bcc6761a9e3d5
Bernardo Cordeiro <41380176+cordeiroP@users.noreply.github.com>
2019-05-24 17:21:53 +0100

    Update FacturasDiaria.hs

--- FacturasDiaria.hs
+++ FacturasDiaria.hs
@@ -3,26 +3,41 @@ import Estrutura
 
 dia::Data->IO()
 dia (d,m,a)= putStrLn(show (d)++"/"++show(m)++"/"++show(a))
-linFact1::LinhaFactura
-linFact1=(1,300000.00,2,2*300000.00)
-linFact2::LinhaFactura
-linFact2=(1,300000.00,2,2*300000.00)
-linFacts::[LinhaFactura]
-linFacts=[linFact1,linFact2]
 
 linFactP::[LinhaFactura]->Total
 linFactP []=0.00
 linFactP ((_,_,_,t):xs)=t+linFactP xs
 
---type Factura = (Codigo, LinhasFacturas, Total, ValorPago, Troco, Data)
-factu1=(1,linFacts,linFactP (linFacts),1000000.00,1000,(11,11,2011))
-factu2=(1,linFacts,linFactP (linFacts),1000000.00,1000,(11,11,2019))
-factus=[factu1,factu2]
 factData::Data->Facturas->Factura
 --factData dat [[]]=[]
 factData dat ((c,l,s,t,r,datF):xs)= if(dat==datF) then (c,l,s,t,r,datF) else factData dat xs 
 
 buscarFactD::Data->Factura
 buscarFactD dat=factData dat factus  
+
+
+codigo (a,_,_,_,_,_,_,_) = a
+nome (_,a,_,_,_,_,_,_) = a
+preco (_,_,a,_,_,_,_,_) = a
+quantidade (_,_,_,a,_,_,_,_) = a
+subtotal (_,_,_,_,a,_,_,_) = a
+total (_,_,_,_,_,a,_,_) = a
+valorPago (_,_,_,_,_,_,a,_) = a
+troco (_,_,_,_,_,_,_,a) = a
+        
+        
+        
+listarFactura :: FacturasFich -> IO ()
+listarFactura [] = putStr ""
+listarFactura (x:xs) = do
+            putStrLn ("Codigo: "++(show (codigo x))++"\tNome: "++(show (nome x))++"\tPreço: "++(show (preco x))++"\tQuantidade: "++(show (quantidade x))++"\tSub total: "++(show (subtotal x))++"\tTotal a pagar: "++(show (total x))++"\tValor pago: "++(show (valorPago x))++"\tTroco: "++(show (troco x)))
+            putStrLn "_________________________________________________________________________________________________________"
+            listarFactura xs
+                        
+tecla::IO()
+tecla = do
+            putStrLn "Digite 1 para sair"
+            a<- getLine
+            putStrLn a
 
 

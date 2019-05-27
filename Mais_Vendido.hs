aa309824220d83ef700030636849b7caa73e50f0
Celestino Langa <celestinolanga11@gmail.com>
2019-05-27 18:46:09 +0100

    bwedelinhas eliminado

--- Mais_Vendido.hs
+++ Mais_Vendido.hs
@@ -1,60 +1,5 @@
 module Mais_Vendido where
-
 import Estrutura
-
-linha1=(2,3000.456,5,3000.456)
-linha2=(3,5000.456,1,5000.456)
-linha3=(4,7000.456,1,7000.456)
-linha4=(2,3000.456,7,3000.456)
-linha5=(5,9000.456,35,9000.456)
-linha6=(2,3000.456,9,3000.456)
-linha7=(6,3000.456,10,3000.456)
-linha8=(5,9000.456,1,9000.456)
-linha9=(2,3000.456,3,3000.456)
-
-linh1=(2,3000.456,9,3000.456)
-linh2=(3,5000.456,15,5000.456)
-linh3=(4,7000.456,1,7000.456)
-linh4=(2,3000.456,14,3000.456)
-linh5=(5,9000.456,35,9000.456)
-linh6=(2,3000.456,19,3000.456)
-linh7=(6,3000.456,100,3000.456)
-linh8=(5,9000.456,1,9000.456)
-linh9=(2,3000.456,3,3000.456)
-
-lin1=(2,3000.456,15,3000.456)
-lin2=(3,5000.456,15,5000.456)
-lin3=(4,7000.456,1,7000.456)
-lin4=(2,3000.456,25,3000.456)
-lin5=(5,9000.456,850,9000.456)
-lin6=(2,3000.456,19,3000.456)
-lin7=(6,3000.456,35,3000.456)
-lin8=(5,9000.456,1,9000.456)
-lin9=(2,3000.456,3,3000.456)
-
-li1=(2,3000.456,95,3000.456)
-li2=(3,5000.456,15,5000.456)
-li3=(4,7000.456,1000,7000.456)
-li4=(2,3000.456,25,3000.456)
-li5=(5,9000.456,35,9000.456)
-li6=(2,3000.456,19,3000.456)
-li7=(6,3000.456,35,3000.456)
-li8=(5,9000.456,1,9000.456)
-li9=(2,3000.456,3,3000.456)
-
-
-
-
-
-
-
-
-bwedeLinhas=[linha1,linha2,linha3,linha4,linha5,linha6,linha7,linha8,linha9]
-bwedeLinhas1=[linh1,linh2,linh3,linh4,linh5,linh6,linh7,linh8,linh9]
-bwedeLinhas2=[lin1,lin2,lin3,lin4,lin5,lin6,lin7,lin8,lin9]
-bwedeLinhas3=[li1,li2,li3,li4,li5,li6,li7,li8,li9]
-
-
 
 
 
@@ -89,5 +34,5 @@ filtro::(Codigo, PrecoUnitario, Quantidade, Subtotal)->(Codigo, PrecoUnitario, Q
 filtro (z,b,c,d) (e,f,g,h)|c>=g     = [(z,b,c,d)]
                           |otherwise= [(e,f,g,h)]
 
-chama::[LinhaFactura]    --faz  a chamada de todas as funçoes
-chama =varredura (trata (maior (impr bwedeLinhas))) bwedeLinhas
+--chama::[LinhaFactura]    --faz  a chamada de todas as funçoes
+--chama = varredura (trata (maior (impr bwedeLinhas))) bwedeLinhas

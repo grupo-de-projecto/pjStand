d037f67404ceebb359e86d92c50a3d9f58bba14b
Celestino Langa <celestinolanga11@gmail.com>
2019-05-28 18:26:57 +0100

    correção de erro

--- CarregarFicheiro.hs
+++ CarregarFicheiro.hs
@@ -2,7 +2,6 @@ module CarregarFicheiro where
 import System.IO
 import Estrutura --hiding(Categoria)
 import Dados
-import Mais_Vendido
 --import DadosFactura
 
 
@@ -15,19 +14,13 @@ lerFile=do
     --    listA=automoveis l
     let lF=map words (lines (transf fF))
         listF=facturaFi lF
-        lis=maisVendido lF
+        --lis=maisVendido lF
 
     --listar listA
-    print lis
+    --print lis
     putStrLn("")
 
---Mostrar Carro mais Vendido
-chamar::IO()
-chamar = do
-          fF<-readFile "factura_todas.txt"
-          let lF = map words (lines (transf fF))
-          print (varredura (trata (maior (impr (maisVendido lF)))) (maisVendido lF))
-          putStrLn ("")
+
 
 
 
@@ -72,14 +65,3 @@ facturaFi ([c,nc,cp,na,p,k,s,t,v,r,tp,m,ca,tl,da]:xs)=let c2=(read c)::Codigo
                     in (c2,nc,cp2,na,p2,k2,s2,t2,v2,r2,tp2,m2,ca2,tl2,da):(facturaFi xs)
 
 facturaFi _=[]
-
-
-maisVendido::[[String]]->LinhasFacturas
-maisVendido ([_,_,cp,_,p,k,s,_,_,_,_,_,_,_,_]:xs)=let     cp2=(read cp)::Codigo
-                                                          p2=(read p)::PrecoUnitario
-                                                          k2=(read k)::Quantidade
-                                                          s2=(read s)::Subtotal
-
-
-                    in (cp2,p2,k2,s2):(maisVendido xs)
-maisVendido _=[]
